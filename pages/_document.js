46ccc44b9476bdb3e0534e6fdfb37c7ae67b0a85
Tim Lucas <t@toolmantim.com>
2017-09-17 21:38:38 +1000

    Update Styled Components to follow latest example

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,26 @@
-
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import * as theme from '../theme'
 
 export default class MyDocument extends Document {
-  render () {
+  static getInitialProps ({ renderPage }) {
     const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
     const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
 
+  render () {
     return (
       <html>
         <Head>
           <title>Buildkite</title>
           <style dangerouslySetInnerHTML={{ __html: theme.root }} />
-          {styleTags}
+          {this.props.styleTags}
         </Head>
         <body>
           <div className='root'>
-            {main}
+            <Main />
           </div>
           <NextScript />
         </body>
