488c56ddb9ca03ae08d5e760b3bd40e273237b08
Tim Lucas <t@toolmantim.com>
2017-09-29 17:19:07 +1000

    Add lang
    Lighthouse accessibility recommendation

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
 
   render () {
     return (
-      <html>
+      <html lang="en-US">
         <Head>
           <meta name="robots" content="noindex" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
