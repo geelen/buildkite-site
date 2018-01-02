6ed2fc65f9ad82506ce3b14437336294613bdb57
Jessica Stokes <hello@jessicastokes.net>
2018-01-02 12:46:13 +1100

    Port frontend's Popover and Dropdown to styled-transition components

---
+++ components/Popover/anchored.js
@@ -0,0 +1,163 @@
+// @flow
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Popover from '.';
+import calculateViewportOffsets from './calculate-viewport-offsets';
+
+type Props = {
+  children: React$Node,
+  className?: string,
+  nibOffsetX: number,
+  position: 'relative' | 'absolute',
+  style?: Object,
+  width: number
+};
+
+type State = {
+  offsetX: number,
+  offsetY: number,
+  showing: boolean,
+  width: number
+};
+
+export default class AnchoredPopover extends React.PureComponent<Props, State> {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    nibOffsetX: PropTypes.number.isRequired,
+    position: PropTypes.oneOf(['relative', 'absolute']).isRequired,
+    style: PropTypes.object,
+    width: PropTypes.number.isRequired
+  };
+
+  static defaultProps = {
+    nibOffsetX: 0,
+    position: 'relative',
+    style: {},
+    width: 250
+  };
+
+  state = {
+    offsetX: 0,
+    offsetY: 45,
+    showing: false,
+    width: 250
+  };
+
+  wrapperNode: ?HTMLSpanElement;
+  popupNode: ?HTMLElement;
+  _resizeDebounceTimeout: ?number;
+
+  handleWindowResize = () => {
+    // when hidden, we wait for the resize to be finished!
+    // to do so, we clear timeouts on each event until we get
+    // a good delay between them.
+    const optimizeForHidden = !this.state.showing;
+
+    // when hidden, we wait 2.5 times as long between
+    // recalculations, which usually means a user is
+    // done resizing by the time we do recalculate
+    const debounceTimeout = (
+      optimizeForHidden
+        ? 250
+        : 100
+    );
+
+    if (optimizeForHidden && this._resizeDebounceTimeout) {
+      this._resizeDebounceTimeout = clearTimeout(this._resizeDebounceTimeout);
+    }
+
+    if (!this._resizeDebounceTimeout) {
+      this._resizeDebounceTimeout = setTimeout(this.handleDebouncedWindowResize, debounceTimeout);
+    }
+  };
+
+  handleDebouncedWindowResize = () => {
+    this._resizeDebounceTimeout = clearTimeout(this._resizeDebounceTimeout);
+    this.calculateViewportOffsets();
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize, false);
+    this.calculateViewportOffsets();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+    this._resizeDebounceTimeout = clearTimeout(this._resizeDebounceTimeout); // just in case
+  }
+
+  calculateViewportOffsets = () => {
+    if (this.wrapperNode) {
+      this.setState(calculateViewportOffsets(this.props.width, this.wrapperNode));
+    }
+  };
+
+  handleMouseOver = (event: MouseEvent) => {
+    // NOTE: We have to cast `event.target` to a Node to use with `contains`
+    //       see <https://github.com/facebook/flow/issues/4645>
+    const target: Node = (event.target: any);
+
+    if (this.wrapperNode/*p
+      && this.wrapperNode.firstElementChild
+      && this.wrapperNode.firstElementChild*/.contains(target)) {
+      this.setState({ showing: true });
+    }
+  };
+
+  handleMouseOut = (event: MouseEvent) => {
+    // NOTE: We have to cast `event.target` to a Node to use with `contains`
+    //       see <https://github.com/facebook/flow/issues/4645>
+    const target: Node = (event.target: any);
+
+    if (this.wrapperNode/*
+      && this.wrapperNode.firstElementChild
+      && this.wrapperNode.firstElementChild*/.contains(target)) {
+      this.setState({ showing: false });
+    }
+  };
+
+  renderPopover(children: React$Node) {
+    if (!this.state.showing) {
+      return;
+    }
+
+    const { width, offsetX, offsetY } = this.state;
+    const { nibOffsetX } = this.props;
+
+    return (
+      <Popover
+        offsetX={offsetX}
+        offsetY={offsetY}
+        nibOffsetX={nibOffsetX}
+        innerRef={(popupNode) => this.popupNode = popupNode}
+        width={width}
+      >
+        {children}
+      </Popover>
+    );
+  }
+
+  render() {
+    const { className, position, style } = this.props;
+
+    const [firstChild, ...children] = React.Children.toArray(this.props.children);
+
+    const wrapperStyle = Object.assign({ position }, style);
+
+    return (
+      <span
+        ref={(wrapperNode) => this.wrapperNode = wrapperNode}
+        className={className}
+        style={wrapperStyle}
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+      >
+        {firstChild}
+        {this.renderPopover(children)}
+      </span>
+    );
+  }
+}
---
+++ components/Popover/index.js
@@ -0,0 +1,103 @@
+// @flow
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const upPointingWhiteNibPath = require('../../assets/images/components/Popover/up-pointing-white-nib.svg');
+
+const NIB_WIDTH = 32;
+
+const Wrapper = styled.div`
+  background-color: #fff;
+  border: 1px solid rgb(221, 221, 221);
+  border-radius: 6px;
+  box-shadow: rgba(0, 0, 0, 0.298039) 0px 4px 10px 0px,
+              rgba(0, 0, 0, 0.027451) 0px 0px 0px 1px;
+  box-sizing: border-box;
+  color: rgb(51, 51, 51);
+  line-height: 18px;
+  margin-top: 5px;
+  padding-bottom: 5px;
+  padding-top: 5px;
+  position: absolute;
+  z-index: 3;
+`;
+
+const ScrollZone = styled.div`
+  max-height: 80vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+`;
+
+const Nib = styled.img`
+  position: absolute;
+  /* pointer-events: none; */
+  left: 50%;
+  top: -20px;
+  width: ${NIB_WIDTH}px;
+  height: 20px;
+  z-index: 3;
+`;
+
+type Props = {
+  children: React$Node,
+  nibOffsetX: number,
+  offsetX: number,
+  offsetY: number,
+  style: Object,
+  innerRef: Function,
+  width: number
+};
+
+export default class Popover extends React.PureComponent<Props> {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    nibOffsetX: PropTypes.number.isRequired,
+    offsetX: PropTypes.number.isRequired,
+    offsetY: PropTypes.number.isRequired,
+    style: PropTypes.object.isRequired,
+    innerRef: PropTypes.func.isRequired,
+    width: PropTypes.number.isRequired
+  };
+
+  static defaultProps = {
+    nibOffsetX: 0,
+    offsetX: 0,
+    offsetY: 45,
+    style: {},
+    innerRef() {},
+    width: 250
+  };
+
+  render() {
+    const { children, innerRef, nibOffsetX, offsetX, offsetY: top, style, width, ...props } = this.props;
+
+    const offset = (width / 2) - offsetX;
+
+    const popoverStyles = {
+      left: `calc(50% - ${offset}px)`,
+      top,
+      transformOrigin: `${offset + nibOffsetX}px -15px`,
+      width,
+      zIndex: 3
+    };
+
+    return (
+      <Wrapper
+        innerRef={innerRef}
+        style={Object.assign(popoverStyles, style)}
+        {...props}
+      >
+        <Nib
+          src={upPointingWhiteNibPath}
+          style={{ marginLeft: -(NIB_WIDTH / 2) - offsetX + nibOffsetX }}
+          alt=""
+        />
+        <ScrollZone>
+          {children}
+        </ScrollZone>
+      </Wrapper>
+    );
+  }
+}
