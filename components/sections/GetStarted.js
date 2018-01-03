ee6dd96dad77b1cdeec4498702aad678c9588dcf
Jessica Stokes <hello@jessicastokes.net>
2018-01-03 17:10:50 +1100

    Style up media layouts

--- components/sections/GetStarted.js
+++ components/sections/GetStarted.js
@@ -2,38 +2,45 @@ import Link from 'next/link'
 import styled from 'styled-components'
 
 import Button from 'components/Button'
-import { Grid, Cell } from 'components/Grid'
 
 const image = require('../../assets/images/get-started/get-started.jpg');
 
 const OffscreenH1 = styled.h1`
-  ${({ theme }) => theme.offscreen}
+  ${({ theme }) => theme.offscreen};
 `
 
 const Paragraph = styled.p`
-  ${({ theme }) => theme.textStyles.bodyCopyLarge}
+  ${({ theme }) => theme.textStyles.bodyCopyLarge};
+  margin-bottom: ${({ theme }) => theme.innerSpacing.s1};
 `
 
-const ImageCell = Cell.extend`
-  text-align: right;
+const ImageGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: -${({ theme }) => theme.innerSpacing.s1};
+`
+
+const ImageCell = styled.div`
+  flex: 1 2 300px;
+  padding: ${({ theme }) => theme.innerSpacing.s1};
+  text-align: center;
+`
+
+const TextCell = styled.div`
+  padding-top: ${({ theme }) => theme.innerSpacing.s1};
+  flex: 2 3 300px;
+  padding: ${({ theme }) => theme.innerSpacing.s1};
 `
 
 const Image = styled.img`
   width: 400px;
   max-width: 100%;
-
-  @media (min-width: 350px) {
-    margin-right: ${({ theme }) => theme.innerSpacing.s2};
-  }
-`
-
-const TextCell = Cell.extend`
-  padding-top: ${({ theme }) => theme.innerSpacing.s1};
 `
 
 export default () => (
   <section>
-    <Grid>
+    <ImageGrid>
       <ImageCell>
         <Image src={image} alt="" />
       </ImageCell>
@@ -41,9 +48,11 @@ export default () => (
         <OffscreenH1>Get Started</OffscreenH1>
         <Paragraph>Sign up for free, and connect your first agent.</Paragraph>
         <p>
-          <Link href="/sign-up" passHref><Button primary>Get Started →</Button></Link>
+          <Link href="/sign-up" passHref>
+            <Button primary>Get Started →</Button>
+          </Link>
         </p>
       </TextCell>
-    </Grid>
+    </ImageGrid>
   </section>
 )
