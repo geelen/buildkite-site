cd36c710749585288e987cc5912f33f9569fb7d8
Jessica Stokes <hello@jessicastokes.net>
2018-01-05 14:11:14 +1100

    Maybe this is a better Grid compromise?

--- components/Grid/index.js
+++ components/Grid/index.js
@@ -3,22 +3,27 @@ import styled from 'styled-components'
 export const Grid = styled('div')`
   display: grid;
   grid-gap: ${({ theme }) => theme.innerSpacing.s1};
-  grid-template-columns: ${({ columns }) => {
-    /*
-     * Accept either a number of columns (which will be fixed at all sizes), or
-     * a minimum column width. This must be less than the minimum screen size at
-     * which the component is likely to be displayed. "300px" is a good maximum.
-     */
-    if (typeof columns === 'number') {
-      return `repeat(${columns}, 1fr)`
-    }
+  grid-template-columns: 1fr;
 
-    return `repeat(auto-fit, minmax(${columns}, 1fr))`
-  }};
+  @media (min-width: 300px) {
+    grid-template-columns: ${({ columns }) => {
+      /*
+      * Accept either a number of columns (which will be fixed at all sizes),
+      * or a minimum column width. This must be less than the minimum screen
+      * size at which the component is likely to be displayed.
+      * "280px" is a good maximum.
+      */
+      if (typeof columns === 'number') {
+        return `repeat(${columns}, 1fr)`
+      }
+
+      return `repeat(auto-fit, minmax(${columns}, 1fr))`
+    }};
+  }
 `
 
 Grid.defaultProps = {
-  columns: '300px'
+  columns: '280px'
 }
 
 export const Cell = styled('div')``;
