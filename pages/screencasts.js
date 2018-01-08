9a830e3440463e2802085a637d2f098f85d5f77e
Tim Lucas <t@toolmantim.com>
2018-01-08 17:41:58 +1100

    I know what we need. Our own <Br>!

--- pages/screencasts.js
+++ pages/screencasts.js
@@ -1,11 +1,15 @@
 import Page, { page } from 'components/Page'
+import Br from 'components/Br'
 
 export default page(({ loggedIn }) => (
   <Page
     headTitle="Buildkite Screencasts"
     title="Screencasts"
-    description="TODO"
+    description={<span>See how Buildkite works in production, <Br maxWidth='30em'/>with this short series of 90 second videos.</span>}
     loggedIn={loggedIn}
   >
+    <div>
+      <h2></h2>
+    </div>
   </Page>
 ))
