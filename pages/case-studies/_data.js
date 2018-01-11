57040ca8f2c56295889e6ba00f5e9325e95785c4
Tim Lucas <t@toolmantim.com>
2018-01-11 13:17:24 +1100

    Add REA case study outline

--- pages/case-studies/_data.js
+++ pages/case-studies/_data.js
@@ -49,6 +49,58 @@ export const shopify = {
   </React.Fragment>
 }
 
+const reaLogo = require('../../assets/images/case-studies/rea/logo.png');
+const reaPhotoPeople = require('../../assets/images/case-studies/rea/people.jpg');
+const reaPhotoOffice = require('../../assets/images/case-studies/rea/office.jpg');
+const reaTestimonialPhoto = require('../../assets/images/case-studies/rea/testimonial-headshot.jpg');
+
+export const rea = {
+  pathname: '/case-studies/rea',
+  team: 'REA Group',
+  link: { label: 'realestate.com.au', url: 'https://realestate.com.au/' },
+  headTitle: 'REA Group CI Buildkite Case Study',
+  logoImage: reaLogo,
+  photoOne: { image: reaPhotoPeople, alt: "TODO" },
+  photoTwo: { image: reaPhotoOffice, alt: "TODO" },
+  indexBackgroundImage: reaPhotoOffice,
+  industry: 'Ecommerce',
+  teamSize: '900 engineers',
+  testimonial: {
+    name: 'A Person',
+    title: 'A persons title',
+    testimonial: 'A testimonial about how Buildkite has supported their organization through growth, and has been amazing for xyz.',
+    headshotImage: reaTestimonialPhoto,
+  },
+  results: [
+    { number: 75, unit: '%', label: 'reduction in build wait times' },
+    { number: 4, unit: '⨉', label: 'increase in CI speed for the same budget' }
+  ],
+  summaryWords: `
+    A short case story about the problem, the solution, the results. Li
+    Europan lingues es membres del sam familie. Lor separat existentie es un
+    myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.
+    Li lingues differe solmen in li grammatica, li pronunciation e li plu
+    commun vocabules.
+  `,
+  words: <React.Fragment>
+    <p>A short case story about the problem, the solution, the results.  Li
+    Europan lingues es membres del sam familie. Lor separat existentie es un
+    myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.
+    Li lingues differe solmen in li grammatica, li pronunciation e li plu
+    commun vocabules.</p>      
+    <p>Omnicos directe al desirabilite de un nov lingua
+    franca: On refusa continuar payar custosi traductores. At solmen va esser
+    necessi far uniform grammatica, pronunciation e plu sommun paroles. Li
+    lingues differe solmen in li grammatica, li pronunciation e li plu commun
+    vocabules.</p>
+    <p>Omnicos directe al desirabilite de un nov lingua franca: On
+    refusa continuar payar custosi traductores. At solmen va esser necessi far
+    uniform grammatica, pronunciation e plu sommun paroles. Li lingues differe
+    solmen in li grammatica.</p>
+  </React.Fragment>
+}
+
 export default [
-  shopify
+  shopify,
+  rea
 ]
