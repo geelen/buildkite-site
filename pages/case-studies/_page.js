66bc1444c905720ce97f64e0e5a22ec40c4baab8
Jessica Stokes <hello@jessicastokes.net>
2018-01-15 16:38:14 +1100

    Basic structure of header

--- pages/case-studies/_page.js
+++ pages/case-studies/_page.js
@@ -1,35 +1,77 @@
+import styled from 'styled-components'
+
 import Link from 'components/Link'
 import Page, { page } from 'components/Page'
+import { MediaItem, ImageCell as RawImageCell, TextCell } from 'components/MediaItem'
 
 import caseStudies from './_data'
+
+const ImageCell = RawImageCell.extend`
+  flex-grow: 0;
+`
+
+const ResultsCell = ImageCell.extend`
+  display: flex;
+  flex-direction: column;
+  text-align: initial;
+  padding: ${({ theme }) => theme.innerSpacing.s1} 0;
+  margin: -${({ theme }) => theme.innerSpacing.s1};
+`
+
+const ResultItem = styled.div`
+  background-color: ${({ theme }) => theme.colors.backgrounds.grey};
+  padding: ${({ theme }) => theme.innerSpacing.s1};
+  margin: ${({ theme }) => theme.innerSpacing.s1};
+`
+
+const Logo = styled.img`
+  width: 300px;
+  max-width: 100%;
+`
 
 export default function caseStudyPage(pathname) {
   const caseStudy = caseStudies.find((caseStudy) => caseStudy.pathname == pathname);
 
   return page(({ loggedIn }) => (
     <Page
+      title="Case Studies"
       headTitle={caseStudy.headTitle}
       loggedIn={loggedIn}
     >
-      <p>
-        <Link href="/case-studies">
-          <a>Case Studies</a>
-        </Link>
-      </p>
-            
-      <h1>{caseStudy.team}</h1>
+      <p>Case Study</p>
 
-      <p>
-        <Link href={caseStudy.link.url}>
-          <a>{caseStudy.link.label}</a>
-        </Link>
-      </p>
+      <MediaItem>
+        <TextCell>
+          <h1>
+            <Link href={caseStudy.link.url}>
+              <a>{caseStudy.team}</a>
+            </Link>
+          </h1>
+          <p>{caseStudy.industry} - {caseStudy.teamSize}</p>
+        </TextCell>
 
-      <p>{caseStudy.industry} - {caseStudy.teamSize}</p>
-      
-      <hr/>
+        <ImageCell>
+          <Logo
+            src={caseStudy.logoImage}
+            alt={`${caseStudy.team} logo`}
+          />
+        </ImageCell>
+      </MediaItem>
 
-      {caseStudy.words}
+      <MediaItem>
+        <TextCell>
+          {caseStudy.words}
+        </TextCell>
+
+        <ResultsCell>
+          {caseStudy.results.map((result, index) => (
+            <ResultItem key={index}>
+              {result.number}{result.unit}<br/>
+              {result.label}
+            </ResultItem>
+          ))}
+        </ResultsCell>
+      </MediaItem>
 
       <hr/>
 
@@ -46,17 +88,6 @@ export default function caseStudyPage(pathname) {
           </cite>
         </footer>
       </blockquote>
-
-      <hr/>
-
-      {caseStudy.results.map((result, index) => (
-        <div key={index}>
-          {result.number}
-          {result.unit}
-          {' '}
-          {result.label}
-        </div>
-      ))}
 
       <hr/>
 
