d0a03d30c00b95c61551d58c6219b9ce1e1bb6d4
Tim Lucas <t@toolmantim.com>
2018-02-01 22:05:05 +1100

    Style the homepage content

--- components/sections/Screencasts.js
+++ components/sections/Screencasts.js
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 
 import Br from 'components/Br'
 import Button from 'components/Button'
-import CenteredLink from 'components/CenteredLink'
+import Link from 'components/Link'
 import { OffscreenH1 } from 'components/OffscreenHeading'
+
+const stepTypesThumbnail = require('../../assets/images/screencasts/step-types-frame.thumbnail.jpg');
 
 const Section = styled.section`
   margin-top: ${({ theme }) => theme.outerSpacing.s2};
@@ -17,14 +19,39 @@ const Paragraph = styled.p`
   text-align: center;
 `
 
+const CenteredButton = Button.withComponent('span').extend`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const ScreencastLinkContainer = styled.a`
+  display: inline-block;
+  position: relative;
+  white-space: nowrap;
+  :hover ${CenteredButton} {
+    background-color: ${({ theme }) => theme.colors.backgrounds.blackHover};
+  }
+`
+
+const ScreencastImage = styled.img`
+  ${({ theme }) => theme.images.screenshots}
+  max-width: 400px;
+  margin: 0 auto;
+`
+
 export default () => (
   <Section>
     <OffscreenH1>Screencasts</OffscreenH1>
     <Paragraph>
-      See how Buildkite works in production, <Br maxWidth='30em'/>with our short series of 90 second videos.
+      See how it works in production, <Br maxWidth='30em'/>with our short series of 90 second videos…
     </Paragraph>
-    <CenteredLink href="/screencasts">
-      <Button>View the screencasts</Button>
-    </CenteredLink>
+    <Link href="/screencasts">
+      <ScreencastLinkContainer>
+        <ScreencastImage src={stepTypesThumbnail} />
+        <CenteredButton>View the screencasts</CenteredButton>
+      </ScreencastLinkContainer>
+    </Link>
   </Section>
 )
