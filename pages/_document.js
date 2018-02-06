0cb34cdcb1197c21c676d55aa604e92ccf000885
Jessica Stokes <hello@jessicastokes.net>
2018-02-05 16:22:38 -0800

    Plug in Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,21 @@ export default class MyDocument extends Document {
     return (
       <html lang="en-US">
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-43649972-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-43649972-1');
+              `
+            }}
+          />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
           <meta name="theme-color" content="#00D974" />
         </Head>
