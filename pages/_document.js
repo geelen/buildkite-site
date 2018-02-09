7f634f9e9f740b591d9a3b03e4017739ce660367
Jessica Stokes <hello@jessicastokes.net>
2018-02-08 17:17:45 -0800

    Google Tag Manager Don't Read This
    Google Analytics Dot Js, Hello

--- pages/_document.js
+++ pages/_document.js
@@ -15,21 +15,6 @@ export default class MyDocument extends Document {
     return (
       <html lang="en-US">
         <Head>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-43649972-1"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'UA-43649972-1');
-              `
-            }}
-          />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
           <meta name="theme-color" content="#00D974" />
         </Head>
@@ -38,6 +23,19 @@ export default class MyDocument extends Document {
             <Main />
           </div>
           <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-43649972-1', 'auto');
+                ga('send', 'pageview');
+              `
+            }}
+          />
         </body>
       </html>
     )
