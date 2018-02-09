daf7c3d88a6da100dcddeef4f9b5c9794ca67193
Jessica Stokes <hello@jessicastokes.net>
2018-02-09 13:45:10 -0800

    Add bundle analyzer step

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+/* global __dirname */
+const path = require('path')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZE } = process.env
+
 // These are used to export pages using Next’s built in `yarn export`. The
 // exports are then picked up by percy.io for screenshots
 const urlPathsToExport = [
@@ -9,6 +14,17 @@ const urlPathsToExport = [
 ]
 
 module.exports = {
+  webpack: function(config) {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: path.resolve(__dirname, 'bundle-analysis/bundle-analyzer-report.html'),
+        openAnalyzer: false
+      }))
+    }
+
+    return config
+  },
   exportPathMap: function() {
     return urlPathsToExport.reduce(function(acc, path) {
       acc[path] = { page: path }
