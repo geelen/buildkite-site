62eccc1383614ca2ee05049a53fd65ef68eb556d
Tim Lucas <t@toolmantim.com>
2018-03-10 16:03:01 +1100

    Remove unused test deps

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// Fixes for:
-// https://github.com/airbnb/enzyme/issues/928
-// https://github.com/facebook/jest/issues/4545
-module.exports = {
-  setupFiles: ["./.jest/shim.js", "./.jest/setup.js"]
-}
