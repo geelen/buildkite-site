bb93bbbadfec23ad2da2c8d00cb2ad926bfcb573
Tim Lucas <t@toolmantim.com>
2018-06-26 15:17:31 +1000

    Add our values

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,7 +5,7 @@ import { ActionGrid, ActionGridItem } from 'components/ActionGrid'
 import Br from 'components/Br'
 import Callout from 'components/Callout'
 import { Grid } from 'components/Grid'
-// import { Masonry, Brick } from 'components/Masonry'
+import { Masonry, Brick } from 'components/Masonry'
 import { MediaItem, ImageCell, TextCell } from 'components/MediaItem'
 import { OffscreenH2 } from 'components/OffscreenHeading'
 import Page, { page } from 'components/Page'
@@ -102,10 +102,10 @@ const HeadOfficeLocation = {
   t: 'r' // eslint-disable-line id-length
 }
 
-// const ValueParagraph = styled.p`
-//   color: ${({ theme }) => theme.colors.text.subdued};
-//   padding: ${({ theme }) => theme.textSpacing.s1} 0;
-// `
+const ValueParagraph = styled.p`
+  color: ${({ theme }) => theme.colors.text.subdued};
+  padding: ${({ theme }) => theme.textSpacing.s1} 0;
+`
 
 export default page((props) => (
   <Page
@@ -175,65 +175,53 @@ export default page((props) => (
       </MediaItem>
     </Section>
 
-    {/* <Section>
+    <Section>
       <SectionHeader>Our Values</SectionHeader>
       <Masonry>
         <Brick>
           <h2>Transparency</h2>
           <ValueParagraph>
-            We believe good work happens out in the open. Internally we believe it’s important to .
-          </ValueParagraph>
-          <ValueParagraph>
-            Open-source, company growth / success / plans internally. Open and frank discussion about security, our infrastructure and how we plan to grow and improve areas of both strength and weakness.
+            We default to working in the open, allowing everyone to understand context and contribute. We're open about both our strengths and weaknesses.
           </ValueParagraph>
         </Brick>
         <Brick>
           <h2>Quality</h2>
           <ValueParagraph>
-            We think that developers need high-quality tools to build amazing products.
+            We think developers need high-quality tools to build amazing products. We take the time to create things with high levels of design, utility, and support.
           </ValueParagraph>
         </Brick>
         <Brick>
           <h2>Diversity</h2>
           <ValueParagraph>
-            We believe that mono-cultures are bad. All the diversity.
+            We believe diversity of experience and thought leads to better solutions, and creates more sustainable cultures. The tools we build must be accessible to people of all abilities and backgrounds.
           </ValueParagraph>
         </Brick>
         <Brick>
           <h2>Independence</h2>
           <ValueParagraph>
-            We are owned by employees and funded by our local developer community, we intend to keep the company focused on providing an amazing product for these people and by these people.
+            We are majority owned by employees, and funded by our local developer community. We own our destiny, and build long term relationships with our customers.
           </ValueParagraph>
         </Brick>
         <Brick>
           <h2>Empathy</h2>
           <ValueParagraph>
-            Really great support, not just listening to customers (although we do that), but fixing things proactively and trying to deeply understand where people are coming from and what they are trying to accomplish and then being a strong partner in that.
+            We believe empathy towards each other, customers and the community, creates the best environment for learning, change and inclusivity.
           </ValueParagraph>
         </Brick>
         <Brick>
           <h2>Collaboration</h2>
           <ValueParagraph>
-            Our platform is a story that is always being written, and we love contributions from outside the Buildkite team. This is reflected in our commitment to open-source as well...
-          </ValueParagraph>
-          <ValueParagraph>
-            We want to know how you are using our platform, and we want to work with you to make it better!
-          </ValueParagraph>
-          <ValueParagraph>
-            We also like to support other projects / tools and conferences that do amazing things.
-          </ValueParagraph>
-          <ValueParagraph>
-            design of platform to allow external integration and customization.
+            Our platform is a story that is always being written, and we encourage contributions from outside the Buildkite team. We actively support and collaborate with other organizations to better our industry, and the world.
           </ValueParagraph>
         </Brick>
         <Brick>
           <h2>Sustainable Growth</h2>
           <ValueParagraph>
-            We believe that great companies grow sustainably. We want to build a company that grows at a pace that we can ensure sticks to our values. We believe that work/life balance and enjoying the work you do is really important. Commercial success is one thing, but we believe it shouldn’t come at the expense of the humans that make it possible.
+            We believe that great companies grow sustainably, respect life outside work, maintain a healthy culture, and limit their impact on the environment.
           </ValueParagraph>
         </Brick>
       </Masonry>
-    </Section> */}
+    </Section>
 
     <ActionGrid>
       <ActionGridItem
