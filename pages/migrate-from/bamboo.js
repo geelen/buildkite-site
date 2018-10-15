0d1408da0a4df640314b83a87cfd617e59c5d2ef
Tim Lucas <t@toolmantim.com>
2018-10-15 14:55:45 +1100

    Clean up most of the remaining content

--- pages/migrate-from/bamboo.js
+++ pages/migrate-from/bamboo.js
@@ -1,19 +1,20 @@
 import styled from 'styled-components'
 
 import Page, { page } from 'components/Page'
+import { Masonry, Brick } from 'components/Masonry'
+import CaseStudyCallout from 'components/CaseStudyCallout'
+import GetStarted from 'components/sections/GetStarted'
+import Screencasts from 'components/sections/Screencasts'
+
+import { rea } from '../case-studies/_data'
 
 import photoColinPanisset from '../../assets/images/migrate-from/bamboo/colinpanisset.jpg'
 
-const photoJml = "todo"
-const iconSpeedGauge = "todo"
-const iconCloud = "todo"
-const iconLightning = "todo"
-const iconLeaf = "todo"
-const iconHeart = "todo"
-const iconMessage = "todo"
-const iconKey = "todo"
-const iconDownload = "todo"
-const buildkiteCustomers = "todo"
+const SectionHeader = styled.h2`
+  ${({ theme }) => theme.textStyles.secondLevelHeading}
+  margin: ${({ theme }) => theme.textSpacing.s1} 0;
+  text-align: center;
+`
 
 const InlinePhoto = styled.span`
   width: 75px;
@@ -24,32 +25,60 @@ const InlinePhoto = styled.span`
   display: inline-block;
 `
 
+const TestimonialContainer = styled.div`
+  display: flex;
+  margin-top: ${({ theme }) => theme.outerSpacing.s1};
+  margin-bottom: ${({ theme }) => theme.outerSpacing.s1};
+  max-width: 40em;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const TestimonialPhoto = styled.div`
+  flex: none;
+  margin-right: ${({ theme }) => theme.innerSpacing.s1};
+`
+
+const TestimonialWords = styled.p`
+  margin-bottom: ${({ theme }) => theme.textSpacing.s1};
+`
+
+const TestimonialBy = styled.p`
+  ${({ theme }) => theme.textStyles.bodyCopySmall}
+  color: ${({ theme }) => theme.colors.text.subdued};
+`
+
 const Testimonial = ({ words, by, image }) => (
-  <div className="my4 pt3 pb2">
-    <div className="max-width-2 mx-auto flex">
-      <div className="flex-none">
-        <InlinePhoto image={image} />
-      </div>
-      <div className="mx2">
-        <p className="m0 dark-gray" style={{ lineHeight: 1.25 }}>{words}</p>
-        <p className="mt1 mb0 h5 black">— {by}</p>
-      </div>
+  <TestimonialContainer>
+    <TestimonialPhoto>
+      <InlinePhoto image={image} />
+    </TestimonialPhoto>
+    <div>
+      <TestimonialWords>{words}</TestimonialWords>
+      <TestimonialBy>— {by}</TestimonialBy>
     </div>
-  </div>
+  </TestimonialContainer>
 )
 
-const Feature = ({ name, description, icon }) => (
-  <div className="my1 flex px1">
-    <div className="flex-none" style={{ width: 30, paddingRight: 10 }}>
-      <span className="inline-block dark-gray" style={{ padding: 2, opacity: 0.35, marginTop: -2 }}>
-        {icon && <img src={icon} alt={name} />}
-      </span>
-    </div>
-    <div>
-      <h3 className="h4 m0 mb1 regular">{name}</h3>
-      <p className="h5 m0 dark-gray" style={{ lineHeight: 1.4 }}>{description}</p>
-    </div>
-  </div>
+const FeatureContainer = styled.div`
+  margin-top: ${({ theme }) => theme.innerSpacing.s2};
+`
+
+const FeatureName = styled.h3`
+  ${({ theme }) => theme.textStyles.bodyCopy}
+  margin-top: 0;
+  margin-bottom: ${({ theme }) => theme.textSpacing.s1};
+`
+
+const FeatureDescription = styled.p`
+  color: ${({ theme }) => theme.colors.text.subdued};
+`
+
+const Feature = ({ name, description }) => (
+  <FeatureContainer>
+    <FeatureName>{name}</FeatureName>
+    <FeatureDescription>{description}</FeatureDescription>
+  </FeatureContainer>
 )
 
 export default page((props) => (
@@ -58,10 +87,6 @@ export default page((props) => (
     title="Migrate from Bamboo to Buildkite"
     {...props}
   >
-    <div className="center my4 py2">
-      <h1 className="m0 mb1 h2 light letter-spacing-1">Bamboo Cloud Sunset Migration Assistance</h1>
-      <p className="m0 max-width-2 mx-auto" style={{ lineHeight: 1.5 }}>We’re offering free support and an extended 30 day trial to help migrate your projects from Bamboo Cloud before it is discontinued. See our <a href="/docs/guides/migrating-from-bamboo">migration guide</a> or <a href="mailto:support@buildkite.com">email our support team</a> for assistance.</p>
-    </div>
 
     <Testimonial
       image={photoColinPanisset}
@@ -69,97 +94,61 @@ export default page((props) => (
       by="Colin Panisset, Global Infrastructure Lead, REA Group"
     />
 
-    <Testimonial
-      image={photoJml}
-      words="Buildkite is the only platform that’s allowed us to run our mobile and cloud test suites in 5 minutes by scaling pipelines across 1800+ agents"
-      by="Jean-Michel Lemieux, SVP Engineering, Shopify"
-    />
+    <SectionHeader>Eight Things You’ll Love About Buildkite</SectionHeader>
+    <Masonry>
+      <Brick>
+        <Feature
+          name="Unlimited parallelization at no extra cost"
+          description="Elastically scale your build jobs from 1 up to many thousands of agents. Buildkite’s agent architecture gives you the power to increase the productivity of your entire software testing and delivery process."
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="No central server to maintain"
+          description="There’s no central server you have to run and maintain—Buildkite agents are lightweight, easy to manage, and run on any platform. Create shared build infrastructure, or allow each team the freedom to orchestrate CI and CD pipelines based on their needs.'"
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="Powerful pipeline support"
+          description="Configure automated and manual pipelines for Dockerized applications, mobile builds, and existing legacy and on-premise environments. Pipelines can run test scripts, perform deployments, pause for manual intervention, and trigger dependent pipelines; all whilst taking advantage of a distributed artifact and key-value store."
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="Seamlessly support both old and new projects"
+          description="Buildkite’s agents run in almost any environment new or old; Linux, Mac, Windows, virtualized environments, cloud machines, and on-premise. Any toolchain and IDE can be used, so you can have a single place for both legacy systems and exciting new projects."
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="Increased developer happiness"
+          description="Beautiful, live-updating web UI, modular tools that fit with existing workflows and best-practices, and a complete REST and GraphQL API for scripting, automation and chatops control. And with a continously improving and responsive build platform you can ensure your teams stay agile and can take advantage of the latest tools."
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="Integration with your existing tools"
+          description="Buildkite tightly integrates with BitBucket including pull requests, build status and git-flow, has flexible HipChat notifications, and integrates with other SCM platforms and services for seamless migrations."
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="Platform agnostic & secure by default"
+          description="Buildkite has integrated support for Amazon EC2, Google Cloud Platform, Azure, Docker and OS X, but you’re free to run agents on own platforms or systems of choice. And Buildkite’s architecture is secure by default—source code never leaves your network."
+        />
+      </Brick>
+      <Brick>
+        <Feature
+          name="Fast and reliable mobile pipelines"
+          description="As mobile becomes increasingly important so does the speed and reliability of your mobile delivery pipelines. Buildkite lets your scale up your mobile builds using both on-site machines and fleets of managed servers. If a pre-release SDK or OS becomes available you can be up and running with automated builds within minutes."
+        />
+      </Brick>
+    </Masonry>
 
-    <div className="my4 py2">
-      <h2 className="mt0 mb3 h2 center lime light letter-spacing-1">8 Things You’ll Love About Buildkite</h2>
-      <div className="sm-flex">
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Unlimited parallelization at no extra cost"
-            description="Elastically scale your build jobs from 1 up to many thousands of agents. Buildkite’s agent architecture gives you the power to increase the productivity of your entire software testing and delivery process."
-            icon={iconSpeedGauge}
-          />
-        </div>
-        <div className="sm-col-6 px1">
-          <Feature
-            name="No central server to maintain"
-            description="There’s no central server you have to run and maintain—Buildkite agents are lightweight, easy to manage, and run on any platform. Create shared build infrastructure, or allow each team the freedom to orchestrate CI and CD pipelines based on their needs.'"
-            icon={iconCloud}
-          />
-        </div>
-      </div>
-      <div className="sm-flex">
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Powerful pipeline support"
-            description="Configure automated and manual pipelines for Dockerized applications, mobile builds, and existing legacy and on-premise environments. Pipelines can run test scripts, perform deployments, pause for manual intervention, and trigger dependent pipelines; all whilst taking advantage of a distributed artifact and key-value store."
-            icon={iconLightning}
-          />
-        </div>
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Seamlessly support both old and new projects"
-            description="Buildkite’s agents run in almost any environment new or old; Linux, Mac, Windows, virtualized environments, cloud machines, and on-premise. Any toolchain and IDE can be used, so you can have a single place for both legacy systems and exciting new projects."
-            icon={iconLeaf}
-          />
-        </div>
-      </div>
-      <div className="sm-flex">
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Increased developer happiness"
-            description="Beautiful, live-updating web UI, modular tools that fit with existing workflows and best-practices, and a complete REST and GraphQL API for scripting, automation and chatops control. And with a continously improving and responsive build platform you can ensure your teams stay agile and can take advantage of the latest tools."
-            icon={iconHeart}
-          />
-        </div>
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Integration with your existing tools"
-            description="Buildkite tightly integrates with BitBucket including pull requests, build status and git-flow, has flexible HipChat notifications, and integrates with other SCM platforms and services for seamless migrations."
-            icon={iconMessage}
-          />
-        </div>
-      </div>
-      <div className="sm-flex">
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Platform agnostic & secure by default"
-            description="Buildkite has integrated support for Amazon EC2, Google Cloud Platform, Azure, Docker and OS X, but you’re free to run agents on own platforms or systems of choice. And Buildkite’s architecture is secure by default—source code never leaves your network."
-            icon={iconKey}
-          />
-        </div>
-        <div className="sm-col-6 px1">
-          <Feature
-            name="Fast and reliable mobile pipelines"
-            description="As mobile becomes increasingly important so does the speed and reliability of your mobile delivery pipelines. Buildkite lets your scale up your mobile builds using both on-site machines and fleets of managed servers. If a pre-release SDK or OS becomes available you can be up and running with automated builds within minutes."
-            icon={iconDownload}
-          />
-        </div>
-      </div>
-    </div>
+    <CaseStudyCallout caseStudy={rea} />
 
-    <div className="my4 py2 center">
-      <h2 className="m0 h2 lime light letter-spacing-1">Used By These Teams</h2>
-      <div className="mt2 px1 relative mx-auto" style={{ maxWidth: "510px" }}>
-        <div className="relative" style={{ paddingBottom: 102.0 / 510.0 * 100 }}>
-          <img src={buildkiteCustomers} alt="Shopify, Digital Ocean, 99designs, Cochlear and Venmo" className="absolute top-0 left-0 col-12" />
-        </div>
-      </div>
-    </div>
-
-    <div className="my4 py2 center">
-      <h2 className="mb2 h2 lime light letter-spacing-1">Pricing</h2>
-      <div className="max-width-2 mx-auto">
-        <p className="mt1 mb1 h3"><span className="h2 letter-spacing-1"><sup className="h4">$</sup>12</span>/developer/month billed yearly</p>
-        <p className="mt1 mb3 dark-gray">or $15/developer billed monthly</p>
-        <p className="my1 dark-gray">Every account includes <strong className="regular black">unlimited pipelines</strong>, <strong className="regular black">unlimited agents</strong>, <strong className="regular black">unlimited builds</strong>, and a free 14 day trial. Open-source, academic, student and non-profit accounts are free.</p>
-      </div>
-    </div>
-
+    <Screencasts />
+    <GetStarted />
   </Page>
 ))
