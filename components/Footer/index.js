30ce55fce2455ce7137431c87c35d853ae473436
Lachlan Hardy <lachlan@lachstock.com.au>
2019-09-25 20:05:21 +1000

    Add Careers link to Footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -76,6 +76,7 @@ export default ({ loggedIn }) => (
             <FooterLink href="/brand-assets">Brand Assets</FooterLink>
             <FooterLink href="/terms-of-service">Terms of Service</FooterLink>
             <FooterLink href="/privacy-policy">Privacy Policy</FooterLink>
+            <FooterLink href="/careers">Careers</FooterLink>
             <FooterLink href="https://shop.buildkite.com/" external>Shop</FooterLink>
           </SmallNav>
         </Cell>
