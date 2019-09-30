4739af1ca71bf899b5bb101628eda316f13ae1f2
Glen Maddern <glenmaddern@gmail.com>
2019-10-01 00:33:17 +1000

    turns out proxying is better

--- fab-server.js
+++ fab-server.js
@@ -1,22 +1,14 @@
-const redirect = url => {
-  return new Response(null, {
-    status: 302,
-    statusText: 'Found',
-    headers: {
-      Location: url
-    }
-  })
-}
+import { fetchAndReturn } from '@fab/compile/utils'
 
 export async function route(settings, path) {
   if (path.match(/lineto-circular-book-\w+.woff2$/)) {
-    return redirect(`https://buildkite.com/_next/static/assets/assets/fonts/lineto-circular-book-0415b07a.woff2`)
+    return fetchAndReturn(`https://buildkite.com/_next/static/assets/assets/fonts/lineto-circular-book-0415b07a.woff2`)
   }
   if (path.match(/lineto-circular-bold-\w+.woff2$/)) {
-    return redirect(`https://buildkite.com/_next/static/assets/assets/fonts/lineto-circular-bold-da46404a.woff2`)
+    return fetchAndReturn(`https://buildkite.com/_next/static/assets/assets/fonts/lineto-circular-bold-da46404a.woff2`)
   }
   if (path.match(/lineto-circular-black-\w+.woff2$/)) {
-    return redirect(`https://buildkite.com/_next/static/assets/assets/fonts/lineto-circular-black-44e65ad5.woff2`)
+    return fetchAndReturn(`https://buildkite.com/_next/static/assets/assets/fonts/lineto-circular-black-44e65ad5.woff2`)
   }
 
   return path
