83e00881031f036ec37c2e2854df9c1010eadb92
Tim Lucas <t@toolmantim.com>
2019-10-24 14:04:28 +1100

    Test the job pages

--- integration-tests/index.test.js
+++ integration-tests/index.test.js
@@ -141,6 +141,8 @@ const pagesToCheck = {
   'About': `/about`,
   'Brand Assets': `/brand-assets`,
   'Careers': `/careers`,
+  'Careers - Senior Product Designer': `/careers/senior-product-designer`,
+  'Careers - Head of Marketing': `/careers/head-of-marketing`,
   'Case Studies - Shopify': `/case-studies/shopify`,
   'Case Studies': `/case-studies`,
   'Contact Us': `/contact`,
